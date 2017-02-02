fafb3720015a2a646a3d389da21c7442c7d09a85
Stefan Baltiyski <stefan.baltiyski@gmail.com>
2017-02-02 12:57:23 +0200

    first commit

--- 04.Java Fundamentals/01.Java Advanced/src/_StacksAndQueues/LabProblems/_02SimpleCalculator.java
+++ 04.Java Fundamentals/01.Java Advanced/src/_StacksAndQueues/LabProblems/_02SimpleCalculator.java
@@ -1,5 +1,29 @@
 package _StacksAndQueues.LabProblems;
 
-public class _02SimpleCalculator {
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.Scanner;
 
+public class _02SimpleCalculator {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String[] tokens = scanner.nextLine().split("\\s+");
+		
+		ArrayDeque<String> calculator = new ArrayDeque<>();
+		Collections.addAll(calculator, tokens);
+		
+		while (calculator.size() > 1) {
+			Integer firstNum = Integer.valueOf(calculator.pop());
+			String operator = calculator.pop();
+			Integer secondNum = Integer.valueOf(calculator.pop());
+			
+			if (operator.equals("+")) {
+				calculator.push(String.valueOf(firstNum + secondNum));
+			} else {
+				calculator.push(String.valueOf(firstNum - secondNum));
+			}
+		}
+		
+		System.out.println(calculator.pop());
+	}
 }
