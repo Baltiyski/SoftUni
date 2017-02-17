c4bc5714bad92e2f73736941413b6bf78d6e4630
Stefan Baltiyski <stefan.baltiyski@gmail.com>
2017-02-17 21:19:45 +0200

    New commit for Stack and Queues

--- 04.Java Fundamentals/01.Java Advanced/src/_StacksAndQueues/HomeProblems/_01ReverseNumbersWithAStack.java
+++ 04.Java Fundamentals/01.Java Advanced/src/_StacksAndQueues/HomeProblems/_01ReverseNumbersWithAStack.java
@@ -1,5 +1,23 @@
 package _StacksAndQueues.HomeProblems;
 
-public class _01ReverseNumbersWithAStack {
+import java.util.ArrayDeque;
+import java.util.Scanner;
 
+public class _01ReverseNumbersWithAStack {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String[] token = scanner.nextLine().split("\\s+");
+		
+		ArrayDeque<Integer> revNumbers = new ArrayDeque<>();
+		
+		for (int i = 0; i < token.length; i++) {
+			revNumbers.push(Integer.parseInt(token[i]));
+		}
+		
+		while (revNumbers.size() > 0) {
+			System.out.print(revNumbers.pop() + " ");
+			
+		}
+		
+	}
 }
